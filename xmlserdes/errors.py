db7614fabfe4617e8e73bd4ca544fcf23dd86832
Ben North <ben.north@sig.com>
2014-12-15 11:07:55 +0000

    XMLSerDesError: Include '/'-joined xpath in str().
    Currently the xpath is always empty, so xpath context is always reported
    as "/".

--- xmlserdes/errors.py
+++ xmlserdes/errors.py
@@ -1,4 +1,10 @@
 class XMLSerDesError(ValueError):
     def __init__(self, *args, **kwargs):
+        self.args = args
+        self.message = args[0]
         self.xpath = kwargs.pop('xpath')
-        ValueError.__init__(self, *args, **kwargs)
+
+    def __str__(self):
+        base_str = ValueError.__str__(self)
+        xpath_str = '/' + '/'.join(self.xpath)
+        return '%s at %s' % (base_str, xpath_str)
