36d220d17212a1a4a9ecb54520b5b7791d328fd3
Ben North <ben.north@sig.com>
2014-12-16 10:26:31 +0000

    Add tag-list comparison classes.

--- xmlserdes/errors.py
+++ xmlserdes/errors.py
@@ -1,3 +1,7 @@
+from difflib import Differ
+from collections import namedtuple
+
+
 class XMLSerDesError(ValueError):
     def __init__(self, *args, **kwargs):
         self.args = args
@@ -8,3 +12,32 @@ class XMLSerDesError(ValueError):
         base_str = ValueError.__str__(self)
         xpath_str = '/' + '/'.join(self.xpath)
         return '%s at %s' % (base_str, xpath_str)
+
+
+class TagDiffEntry(namedtuple('TagDiffEntry', 'type tag')):
+    type_from_code = {'- ': 'missing',
+                      '+ ': 'unexpected',
+                      '  ': 'as-expected'}
+
+    @classmethod
+    def maybe_from_delta_entry(cls, d):
+        code = d[:2]
+        if code == '? ':
+            return None
+        return cls(cls.type_from_code[code], d[2:])
+
+    def __str__(self):
+        return '%s: %s' % self
+
+
+class TagListComparison(object):
+    def __init__(self, expected_tags, got_tags):
+        self.entries = list(filter(None,
+                                   map(TagDiffEntry.maybe_from_delta_entry,
+                                       Differ().compare(expected_tags, got_tags))))
+
+    def __str__(self):
+        return '[%s]' % ', '.join(map(str, self.entries))
+
+    def __len__(self):
+        return len(self.entries)
