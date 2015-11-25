dda5bff69467535ae78ba030ecffb97fbc8f0d3c
Ben North <ben.north@sig.com>
2015-11-25 12:32:05 +0000

    Rename package to 'xml-serdes'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='XMLserdes',
+    name='xml-serdes',
     version='0.1.0',
     packages=find_packages(),
     install_requires=['lxml', 'numpy'],
