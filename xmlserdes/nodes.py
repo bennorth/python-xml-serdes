f6386ea58458f85115f2d3d4cf8885738c5befe3
Ben North <ben@redfrontdoor.org>
2016-10-01 08:12:31 +0100

    XMLAttributeNode: Add core of class

--- xmlserdes/nodes.py
+++ xmlserdes/nodes.py
@@ -14,5 +14,13 @@ class XMLElementNode(object):
         parent_elt.append_child(self.elt)
 
 
+class XMLAttributeNode(object):
+    def __init__(self, tag, text=None):
+        if text is None:
+            raise ValueError('expected "text" when constructing XMLAttributeNode')
+        self.tag = tag
+        self.text = text
+
+
 def make_XMLNode(tag, *args):
     return XMLElementNode(tag, *args)
