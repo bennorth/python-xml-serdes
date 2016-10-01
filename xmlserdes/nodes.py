82aa93f5d82eff8b631c835f7e8ef495039c562e
Ben North <ben@redfrontdoor.org>
2016-10-01 07:56:19 +0100

    XMLElementNode.append_to(): Add method

--- xmlserdes/nodes.py
+++ xmlserdes/nodes.py
@@ -10,6 +10,9 @@ class XMLElementNode(object):
     def append_child(self, ch):
         self.elt.append(ch)
 
+    def append_to(self, parent_elt):
+        parent_elt.append_child(self.elt)
+
 
 def make_XMLNode(tag, *args):
     return XMLElementNode(tag, *args)
