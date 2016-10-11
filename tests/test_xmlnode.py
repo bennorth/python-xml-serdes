304d003a4b344c67a2ee5f182e916eb9aa3847f7
Ben North <ben@redfrontdoor.org>
2016-10-11 22:44:29 +0100

    Test 'append to attribute' gives error

--- tests/test_xmlnode.py
+++ tests/test_xmlnode.py
@@ -1,8 +1,21 @@
 import pytest
 
 import xmlserdes as X
+import xmlserdes.nodes as XN
 
 make_TD = X.TypeDescriptor.from_terse
+
+
+class TestBadAppend(object):
+    @pytest.mark.parametrize(
+        'err_fragment, tag',
+        [('child', 'height'), ('attribute', '@height')])
+    #
+    def test_append_to_attribute(self, err_fragment, tag):
+        n = XN.make_XMLNode('@weight', '23 stone')
+        with pytest.raises_regexp(ValueError, 'cannot append {0}'.format(err_fragment)):
+            ch = XN.make_XMLNode(tag, '6 feet')
+            ch.append_to(n)
 
 
 class TestBadElement(object):
